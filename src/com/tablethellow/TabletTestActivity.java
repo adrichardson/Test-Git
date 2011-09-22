9bfa43c8a1170c4883c7a04aa626ffda2133a0bf
Andrew Richardson <sonicsaw@gmail.com>
2011-09-22 11:03:34 -0400

    Initial Commit

---
+++ src/com/tablethellow/TabletTestActivity.java
@@ -0,0 +1,13 @@
+package com.tablethellow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabletTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
