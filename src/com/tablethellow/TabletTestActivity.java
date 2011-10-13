0c0f997f8d508ddd8fd03d0d4f922667d3e6b0bd
Andrew Richardson <sonicsaw@gmail.com>
2011-10-13 15:42:01 -0400

    Testing the tree activity

--- src/com/tablethellow/TabletTestActivity.java
+++ src/com/tablethellow/TabletTestActivity.java
@@ -4,9 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class TabletTestActivity extends Activity {
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
---
+++ src/com/tablethellow/TreeActivity.java
@@ -0,0 +1,17 @@
+package com.tablethellow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TreeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tree.xml);
+	}
+	
+	public void printTest()
+	{
+		System.out.println("Hello!");
+	}
+}
